1d3c3e825f2e4dd235ac07c7f47368cc5bcced5b
AllenJB <AllenJB@users.noreply.github.com>
2014-11-06 10:15:19 +0000

    File Library: Methods to retrieve the maximum file upload size and convert file sizes between human readable and bytes

--- application/libraries/file.php
+++ application/libraries/file.php
@@ -14,4 +14,48 @@ class File
 
         return $lines;
     }
+
+
+    public function human2bytes($val)
+    {
+        $val = trim($val);
+        $last = strtolower($val[strlen($val) - 1]);
+        switch ($last) {
+            // The 'G' modifier is available since PHP 5.1.0
+            case 'g':
+                $val *= 1024;
+            case 'm':
+                $val *= 1024;
+            case 'k':
+                $val *= 1024;
+        }
+
+        return $val;
+    }
+
+
+    public function bytes2human($val)
+    {
+        if ($val < 1024) {
+            return number_format($val) . ' bytes';
+        } else {
+            if ($val < (1024 * 1024)) {
+                return number_format($val / 1024) . ' KB';
+            }
+        }
+        return number_format($val / (1024 * 1024)) . ' MB';
+    }
+
+
+    /**
+     * Returns the maximum file upload size (taking both upload_max_filesize and post_max_size into account) in bytes.
+     * @return int
+     */
+    public function getUploadMaxFilesize()
+    {
+        $file = $this->human2bytes(ini_get('upload_max_filesize'));
+        $post = $this->human2bytes(ini_get('post_max_size'));
+
+        return ($post < $file ? $post : $file);
+    }
 }
